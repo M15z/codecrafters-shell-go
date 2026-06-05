80bf6d284f63f5ac44baa840fc72e5c847c7fb6c
M15z <smaaz643@gmail.com>
2026-06-05 19:07:32 +0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main() {
+func readPrompt() string {
 	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	return command
+
+}
+
+func main() {
+	command := readPrompt()
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
